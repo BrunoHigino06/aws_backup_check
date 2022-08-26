bd38d5d9060497a3d82bea89962578871d1f30e0
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-26 05:01:43 -0300

    Update aws_backup_check.py

--- aws_backup_check.py
+++ aws_backup_check.py
@@ -9,7 +9,9 @@ def aws_backup_check():
 
     json_response = json.dumps(response, indent=4, sort_keys=True, default=str)
     if json_response is None:
-        print("Any backup funded in the account: "+json_response['BackupJobs']['AccountId'])
+        print("Any backup funded in account: "+json_response['BackupJobs']['AccountId'])
+    else:
+        print("backup funded in account: "+json_response['BackupJobs']['AccountId'])
 
 
 aws_backup_check()
