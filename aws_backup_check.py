ed2cb0c2cdf2cbbaeb9781e5e94033809a33b509
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-26 02:29:54 -0300

    Update aws_backup_check.py

--- aws_backup_check.py
+++ aws_backup_check.py
@@ -3,7 +3,7 @@ import boto3
 def aws_backup_check():
     client = boto3.client('backup')
     response = client.list_backup_jobs(
-        MaxResults=1,
+        MaxResults=1
     )
     print("Teste response: "+response)
 
