1603362753d56584f36c9b784ef9252282610716
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-29 06:12:47 -0300

    Update aws_backup_check.py

--- aws_backup_check.py
+++ aws_backup_check.py
@@ -15,7 +15,7 @@ def aws_backup_check():
                
         if jobId['BackupJobId']:
             print('exist')
-        if 'BackupJobId' not in jobId['BackupJobId']:
+        if jobId['BackupJobId'] not in jobId['BackupJobs']:
             print('not exist')
 
 
