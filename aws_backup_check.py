8019fed72f96bdb45d9d3ed9b46240db29049568
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-29 03:49:51 -0300

    Update aws_backup_check.py

--- aws_backup_check.py
+++ aws_backup_check.py
@@ -2,7 +2,7 @@ import boto3
 import json
 
 def aws_backup_check():
-    client = boto3.client('backup', region_name='sa-east-1')
+    client = boto3.client('backup', region_name='us-east-1')
     response = client.list_backup_jobs(
         MaxResults=1
     )
@@ -14,9 +14,9 @@ def aws_backup_check():
     for jobId in database['BackupJobs']:
                
         if jobId['BackupJobId']:
-            print('No ID')
-        else:
             print('exist')
+        else:
+            print('No ID')
 
 
 
