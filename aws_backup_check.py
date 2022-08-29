400402fcd83319d040d5c37756755c97292d1a54
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-29 07:38:16 -0300

    Update aws_backup_check.py

--- aws_backup_check.py
+++ aws_backup_check.py
@@ -12,7 +12,7 @@ def aws_backup_check():
     database = json.loads(json_response)
     print(database)
 
-    test = True if "BackupJobId" in database["BackupJobs"] else False
+    test = True if "BackupJobId" in database["BackupJobs"][0] else False
 
     print(test)
 
