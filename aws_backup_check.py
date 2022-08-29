e260601f3118e192b968832cf7b1aff69a43b99e
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-29 06:49:36 -0300

    Update aws_backup_check.py

--- aws_backup_check.py
+++ aws_backup_check.py
@@ -10,7 +10,7 @@ def aws_backup_check():
     json_response = json.dumps(response, indent=4, sort_keys=True, default=str)
 
     database = json.loads(json_response)
-
+    print(database)
     if database['BackupJobs']['BackupJobId'] in database['BackupJobs']:
         print('not exist')
     if 'BackupJobId' in database['BackupJobs']:
