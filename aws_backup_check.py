33c2fb409c0ec6d5b10f1a3e76ec9c4e142e003d
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-29 03:37:40 -0300

    Update aws_backup_check.py

--- aws_backup_check.py
+++ aws_backup_check.py
@@ -13,7 +13,7 @@ def aws_backup_check():
 
     for jobId in database['BackupJobs']:
                
-        if jobId['BackupJobs'] == '':
+        if jobId['BackupJobsId'] == '':
             print('No ID')
         else:
             print('exist')
