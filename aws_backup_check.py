d5254d51ff16fd4b403568ef5ea6f5c33a6ebc61
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-30 01:40:22 -0300

    2022.08.30

--- aws_backup_check.py
+++ aws_backup_check.py
@@ -2,15 +2,19 @@ import boto3
 import json
 
 def aws_backup_check(accountid):
-    client = boto3.client('backup', region_name='us-east-1')
-    response = client.list_backup_jobs(
-        MaxResults=1
-    )
+    regions = ['us-east-1', 'sa-east-1']
 
-    json_response = json.dumps(response, indent=4, sort_keys=True, default=str)
+    for region in regions:
 
-    database = json.loads(json_response)
-    try:
-        test = True if "BackupJobId" in database["BackupJobs"][0] else False
-    except:
-        print(accountid)
+        client = boto3.client('backup', region_name=region)
+        response = client.list_backup_jobs(
+            MaxResults=1
+        )
+
+        json_response = json.dumps(response, indent=4, sort_keys=True, default=str)
+
+        database = json.loads(json_response)
+        try:
+            test = True if "BackupJobId" in database["BackupJobs"][0] else False
+        except:
+            print(accountid)
