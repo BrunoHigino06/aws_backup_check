c74b0197884fc6060d7c0b8758f3da97599f237d
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-30 04:50:31 -0300

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -6,6 +6,6 @@ def generate_report():
     with open("accounts.txt") as f:
         for id in f:
             sts_session(id.strip())
-            aws_backup_check(id, os.environ["KEY_ID"], os.environ["ACCESS_KEY"], os.environ["TOKEN"])
+            aws_backup_check(id, os.system('$KEY_ID'), os.system('$ACCESS_KEY'), os.system('$TOKEN'))
 
 generate_report()
