0b69f55ad2895dfe61ab26e3e414df06f179caff
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-30 01:50:44 -0300

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -4,7 +4,7 @@ import aws_backup_check
 def generate_report():
     with open("accounts.txt") as f:
         for id in f:
-            sts_session(id.strip())
-            aws_backup_check(id)
+            sts_session.sts_session(id.strip())
+            aws_backup_check.aws_backup_check(id)
 
 generate_report()
