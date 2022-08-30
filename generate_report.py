619468bdd3a29cc444e2867c3e5099689780fcaa
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-30 07:55:24 -0300

    30.08.2022

--- generate_report.py
+++ generate_report.py
@@ -6,6 +6,5 @@ def generate_report():
     with open("accounts.txt") as f:
         for id in f:
             sts_session(id.strip())
-            aws_backup_check(id, os.getenv('KEY_ID'), os.getenv('ACCESS_KEY'), os.getenv('TOKEN'))
 
 generate_report()
