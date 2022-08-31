0e1c76635be0515e73377e7713c7af35db7da9e7
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-31 05:19:25 -0300

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -2,7 +2,7 @@ import os
 from sts_session import sts_session
 
 def generate_report():
-    print('AccountId,Account Name,Region,Backup?')
+    print('AccountId,Account Name,Region,Backup Job?')
     with open("accounts.txt") as f:
         for id in f:
             sts_session(id.strip())
