2fd865ddc9df4576a4759ffda7e46e55de296420
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-31 05:03:10 -0300

    Update aws_backup_check.py

--- aws_backup_check.py
+++ aws_backup_check.py
@@ -7,17 +7,6 @@ def aws_backup_check(accountid, KEY_ID, ACCESS_KEY, TOKEN):
 
     for region in regions:
 
-        organizations = boto3.client('organizations', region_name=region, aws_access_key_id=KEY_ID, aws_secret_access_key=ACCESS_KEY, aws_session_token=TOKEN)
-        org_response = organizations.describe_account(
-        AccountId=accountid
-        )
-
-        org_json = json.dumps(org_response, indent=4, sort_keys=True, default=str)
-
-        org_database = json.loads(org_json)
-
-        accountname = org_database['Account'][0]['Name']
-
         backup = boto3.client('backup', region_name=region, aws_access_key_id=KEY_ID, aws_secret_access_key=ACCESS_KEY, aws_session_token=TOKEN)
         response = backup.list_backup_jobs(
             MaxResults=1
