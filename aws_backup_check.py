3b104b6a5bc9915678249aa0750737db9301c0cc
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-31 04:21:53 -0300

    31.08.2022

--- aws_backup_check.py
+++ aws_backup_check.py
@@ -2,12 +2,24 @@ import boto3
 import json
 
 def aws_backup_check(accountid, KEY_ID, ACCESS_KEY, TOKEN):
+
+    organizations = boto3.client('organizations', aws_access_key_id=KEY_ID, aws_secret_access_key=ACCESS_KEY, aws_session_token=TOKEN)
+    org_response = organizations.describe_account(
+            AccountId=accountid
+    )
+
+    org_json = json.dumps(org_response, indent=4, sort_keys=True, default=str)
+
+    org_database = json.loads(org_json)
+
+    accountname = org_database['Account'][0]['Name']
+
     regions = ["us-east-1", "sa-east-1"]
 
     for region in regions:
 
-        client = boto3.client('backup', region_name=region, aws_access_key_id=KEY_ID, aws_secret_access_key=ACCESS_KEY, aws_session_token=TOKEN)
-        response = client.list_backup_jobs(
+        backup = boto3.client('backup', region_name=region, aws_access_key_id=KEY_ID, aws_secret_access_key=ACCESS_KEY, aws_session_token=TOKEN)
+        response = backup.list_backup_jobs(
             MaxResults=1
         )
 
@@ -16,6 +28,5 @@ def aws_backup_check(accountid, KEY_ID, ACCESS_KEY, TOKEN):
         database = json.loads(json_response)
         try:
             test = True if "BackupJobId" in database["BackupJobs"][0] else False
-            print('')
         except:
-            print('No backup job found in the region: '+region+' for the account: '+accountid)
+            print(accountid+','+accountname+','+region+'No')
--- generate_report.py
+++ generate_report.py
@@ -3,7 +3,7 @@ from sts_session import sts_session
 from aws_backup_check import aws_backup_check
 
 def generate_report():
-    print('AccountId,Region,Backup?')
+    print('AccountId,Account Name,Region,Backup?')
     with open("accounts.txt") as f:
         for id in f:
             sts_session(id.strip())
