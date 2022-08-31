bf7488dc0364beca3ef0f2a959917f2d5d8ffad0
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-08-31 04:54:02 -0300

    Update aws_backup_check.py

--- aws_backup_check.py
+++ aws_backup_check.py
@@ -3,17 +3,6 @@ import json
 
 def aws_backup_check(accountid, KEY_ID, ACCESS_KEY, TOKEN):
 
-    organizations = boto3.client('organizations', aws_access_key_id=KEY_ID, aws_secret_access_key=ACCESS_KEY, aws_session_token=TOKEN)
-    org_response = organizations.describe_account(
-            AccountId=accountid
-    )
-
-    org_json = json.dumps(org_response, indent=4, sort_keys=True, default=str)
-
-    org_database = json.loads(org_json)
-
-    accountname = org_database['Account'][0]['Name']
-
     regions = ["us-east-1", "sa-east-1"]
 
     for region in regions:
